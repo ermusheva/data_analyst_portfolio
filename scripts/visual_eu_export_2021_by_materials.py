d3b41c7864d4e4677959e841787a1a3552a036bd
ermusheva <ermusheva@gmail.com>
2024-12-27 22:21:46 +0100

    Simple basic bars and plots

---
+++ scripts/visual_eu_export_2021_by_materials.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+# Load result csv file
+eu_export_by_materials = pd.read_csv('..//results//eu_export_by_years_materials.csv')
+
+eu_export_2021 = eu_export_by_materials[eu_export_by_materials['time_period'] == 2021]
+# Replace material names to shorter names
+eu_export_2021.loc[:, 'material_label'] = eu_export_2021['material_label'].replace('Not specified', 'Not spec');
+eu_export_2021.loc[:, 'material_label'] = eu_export_2021['material_label'].replace('Paper and cardboard', 'Paper');
+print(eu_export_2021.head())
+print(len(eu_export_2021))
+
+# Define a colormap
+cmap = plt.get_cmap("plasma", 2)
+
+fig, ax = plt.subplots(figsize=(16, 9))
+bars = ax.barh(eu_export_2021['material_label'], eu_export_2021['tonne'], color = cmap(0), edgecolor = 'black', capsize=7, label=eu_export_2021['tonne'])
+
+ax.set_title('Total export of the European Union in 2021 by materials, Billions tonnes')
+ax.invert_yaxis()
+
+# Add values near the bars
+for bar, value in zip(bars, eu_export_2021['tonne']):
+    ax.text(value + 100000, bar.get_y() + bar.get_height()/2,
+            f'{value / 1e6:.1f}B', va='center', fontsize=10, color='black')
+
+ax.xaxis.set_visible(False)
+
+plt.savefig('..//assets//eu_export_2021_by_materials.png', dpi=300)
+plt.show()
+
+
+
+
+
--- scripts/visual_eu_export_import_by_years.py
+++ scripts/visual_eu_export_import_by_years.py
@@ -7,31 +7,23 @@ from matplotlib.ticker import FuncFormatter
 total_eu_export_import = pd.read_csv('..//results//eu_export_import_by_years.csv')
 
 # Set up the figure and axis
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots(figsize=(16, 9))
 
-
-# width of the bars
-barWidth = 0.3 
-
-export_bars = total_eu_export_import['export_thousand_euros']
-import_bars = total_eu_export_import['import_thousand_euros']
+export_data = total_eu_export_import['export_thousand_euros']
+import_data = total_eu_export_import['import_thousand_euros']
 
 # Define a colormap
 cmap = plt.get_cmap("plasma", 2)
 
-# The x position of bars
-r1 = np.arange(len(export_bars))
-r2 = [x + barWidth for x in r1]
+# The x position
 years = total_eu_export_import['time_period']
- 
-# Create bars
-ax.bar(r1, export_bars, width = barWidth, color = cmap(0), edgecolor = 'black', capsize=7, label='export')
-ax.bar(r2, import_bars, width = barWidth, color = cmap(1), edgecolor = 'black', capsize=7, label='import')
+
+ax.plot(years, export_data, color = cmap(0), label='export')
+ax.plot(years, import_data, color = cmap(1), label='import')
  
 # general layout
-plt.xticks([r + barWidth for r in range(1, len(export_bars), 2)], years[1:len(years):2])
-ax.set_ylabel("Billions \N{euro sign}")
-ax.set_title('Total export and import of the European Union by years, \N{euro sign}')
+plt.xticks(years[1:len(years):2])
+ax.set_title('Total export and import of the European Union by years, Billions \N{euro sign}')
 ax.legend()
 
 # Format the y-axis to show values in billions
@@ -40,7 +32,7 @@ def to_billions(y, pos):
 
 ax.yaxis.set_major_formatter(FuncFormatter(to_billions))
  
-plt.tight_layout()
+# plt.tight_layout()
 plt.savefig('..//assets//eu_export_import_by_years.png', dpi=300)
 plt.show()
 
